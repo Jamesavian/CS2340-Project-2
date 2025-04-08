be4cab8de77a102b2949b0778616456aebec8350
Bates, Kaleb Joshua <kbates39@gatech.edu>
2025-04-08 12:25:00 -0400

    Combined income and expenses into one page

---
+++ report/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.transaction_list, name='report.transaction_list'),
+    path('add_income/', views.add_income, name='report.add_income'),
+    path('add_expense/', views.add_expense, name='report.add_expense'),
+]
+
+#path('signup', views.signup, name='accounts.signup'),
