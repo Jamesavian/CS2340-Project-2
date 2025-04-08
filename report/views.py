ac47c5896d4d1882c17e264cf8b8866cb51eb4a4
Myltsev, Alexander Sergey <amyltsev3@gatech.edu>
2025-04-08 13:04:53 -0400

    Added add new income and add new expense into financial report model

--- report/views.py
+++ report/views.py
@@ -6,8 +6,28 @@ from .forms import ReportForm, IncomeForm, ExpenseForm
 
 @login_required
 def transaction_list(request):
+    income_form = IncomeForm()
+    expense_form = ExpenseForm()
+
+    if request.method == 'POST':
+        if 'income_submit' in request.POST:
+            income_form = IncomeForm(request.POST)
+            if income_form.is_valid():
+                income = income_form.save(commit=False)
+                income.user = request.user
+                income.save()
+                return redirect('transaction_list')
+        elif 'expense_submit' in request.POST:
+            expense_form = ExpenseForm(request.POST)
+            if expense_form.is_valid():
+                expense = expense_form.save(commit=False)
+                expense.user = request.user
+                expense.save()
+                return redirect('transaction_list')
+
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'report/transaction_list.html', {'transactions': transactions})
+    return render(request, 'report/transaction_list.html', {'transactions': transactions,
+                                                            'income_form': income_form, 'expense_form': expense_form})
 
 @login_required
 def add_transaction(request, formType, templateName):
